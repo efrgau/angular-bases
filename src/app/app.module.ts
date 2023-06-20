f63d7051e129f7ee7ad898a2dcd9cfc81fb0b9e9
Edwin Pacheco <edwin.f.pacheco@gmail.com>
2023-06-20 08:00:29 -0500

    creacion de dbz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from "./heroes/heroes.module";
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CounterModule } from './counter/counter.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
