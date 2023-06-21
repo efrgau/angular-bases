089aff9a9642ed62dbbd9b0e7a62ab17739234dc
Edwin Pacheco <edwin.f.pacheco@gmail.com>
2023-06-21 06:58:31 -0500

    Componentes pequeños

---
+++ src/app/dbz/components/character/character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.css']
+})
+export class CharacterComponent {
+
+}
